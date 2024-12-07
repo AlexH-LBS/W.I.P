fa1aba7c933208e500ca189e940c0e1b646d67a6
TPsu24b <theo.palm@elev.ga.lbs.se>
2024-12-07 13:41:04 +0100

    added long music box, made them change color on impact

--- W.I.P/Assets/Scripts/MusicBlocks/PlayerScript.cs
+++ W.I.P/Assets/Scripts/MusicBlocks/PlayerScript.cs
@@ -12,8 +12,12 @@ public class PlayerScript : MonoBehaviour
     //true or false
     public bool touching;
     public bool interaction;
+    public bool longBlock;
+    public bool longBlockScore;
     //score duh
     public int score;
+    //error time for long
+    public float errorTimeForLong = 0.25f;
     //to tell the popularity script hit
     public Popularity Popularity;
     //debug menu shit
@@ -23,42 +27,99 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         //if player hit while touching make it do some shit
-        if (Input.GetKeyDown(input) && touching )
+        if (Input.GetKey(input) && touching)
         {
-            StartCoroutine(InteractedAtMoment());
+            if (longBlock)
+            {
+                interaction = true;
+            }
+            else
+            {
+                StartCoroutine(InteractedAtMoment());
+            }
+        }
+        if (Input.GetKeyUp(input)) {
+            interaction = false;
         }
         //if player pressed when not touching it say miss
         if (Input.GetKeyDown(input) && !touching)
         {
-            print("missed");
+            print("missed " + gameObject.name);
             lastMove.text = "Miss";
         }
     }
-    //not really sure found it on google as an solution
+    //sets interaction to true then false after .05 seconds
     IEnumerator InteractedAtMoment()
     {
         interaction = true;
         yield return new WaitForSecondsRealtime(0.05f);
         interaction = false;
+
+    }
+    IEnumerator LongBlockInteraction(Collider2D collision)
+    {
+        //wait .25 seconds before cheaking if interaction is still not happening(error time)
+        yield return new WaitForSecondsRealtime(errorTimeForLong);
+
+        if (!interaction)
+        {
+            //incase the long block dosent delete it self
+            if(collision != null){
+                Destroy(collision.gameObject);
+            }
+            longBlockScore = false;
+            lastMove.text = "Missed Long";
+        }
+        if (interaction){
+            longBlockScore = true;
+        }
     }
     //update loop but only when touching block
     public void OnTriggerStay2D(Collider2D collision)
-    {
-        //the thing that destroys the block and adds scores
-        touching = true;
-        if (interaction){
-            score += 1;
-            print("YOU SCORE");
-            //I added so that the hitmusic is mulitplied by 1%, probably doesnt work but you get roughly the point. is it possible to make the score and fame/hitmusic the same so we write less code and variables?
-            Popularity.fame(1);
-            text.text = score.ToString();
-            Destroy(collision.gameObject);
-            lastMove.text = "Hit";
+        {
+            //the thing that destroys the block and adds scores
+            touching = true;
+            //cheakcs if its long 
+            if (collision.gameObject.tag == "LongMusicBlock")
+            {
+                longBlock = true;
+            }
+            if (interaction && longBlock) {
+                lastMove.text = "Hiting Long";
+                longBlockScore = true;
+            }
+            //if you miss the long block 
+            if (!interaction && longBlock) {
+                StartCoroutine(LongBlockInteraction(collision));
+
+            }
+            //score thingy
+            if (interaction && !longBlock) {
+                score += 1;
+                print("YOU SCORE");
+                //I added so that the hitmusic is mulitplied by 1%, probably doesnt work but you get roughly the point. is it possible to make the score and fame/hitmusic the same so we write less code and variables?
+                Popularity.fame(1);
+                text.text = score.ToString();
+                Destroy(collision.gameObject);
+                lastMove.text = "Hit";
+            }
         }
-    }
-    //when collsion ends touching ends
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        touching= false;
-    }
+        //when collsion ends touching ends
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            touching = false;
+            longBlock = false;
+        //score long block
+        if (longBlockScore){
+            score += 4;
+            print("YOU SCORE LONG");
+            Popularity.fame(2);
+            text.text = score.ToString();
+        }
+        if (interaction && longBlock) {
+                lastMove.text = "Hit end long";
+            }
+        }
+
+    
 }
