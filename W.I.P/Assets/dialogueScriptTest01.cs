bdc04dd0e99239e0d3f8fc70d98054057ee6215b
AlexH-LBS <alexander.holmstedt@elev.ga.lbs.se>
2024-12-09 20:28:37 +0100

    Dialogue Overhaul

--- W.I.P/Assets/dialogueScriptTest01.cs
+++ W.I.P/Assets/dialogueScriptTest01.cs
@@ -4,63 +4,45 @@ using UnityEngine;
 
 public class dialogueScriptTest01 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool IsStartingStatement;
+    public GameObject dialogueOption1;
+    public GameObject dialogueOption2;
+    public GameObject dialogueOption3;
     void Start()
     {
-        print("script start");
+        Statement();
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        //string choice=Dialogue("hi", "Greetings", "eh");
-        //print(choice);
-    }
+        if (IsStartingStatement == false)
+        {
 
-    //change from void to string later
-    void Dialogue(string Answer0, string Answer1, string Answer2)
-    {
-        
-        int choice=0;
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            choice += 1;
-            print(choice);
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            choice -= 1;
-            print(choice);
-        }
-        if (choice >= 3)
-        {
-            choice = 0;
-            print(choice);
-        }
-        if (choice <= 0)
-        {
-            choice = 3;
-            print(choice);
-        }
-        if (Input.GetKeyDown(KeyCode.KeypadEnter))
-        {
-            if (choice == 0)
-            {
-                //return Answer0;
-            }
-            if (choice == 1)
-            {
-                //return Answer1;
-            }
-            if (choice == 0)
-            {
-                //return Answer2;
-            }
-
-
             
         }
-        
-        
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+
+        }
+
+
+
+
+
+    }
+
+    void Statement()
+    {
+        print("Statement");
     }
 }
