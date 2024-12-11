70fceb46616a6f5ac728a6a61e45a4f3c22e3ff4
AlexH-LBS <alexander.holmstedt@elev.ga.lbs.se>
2024-12-11 12:31:22 +0100

    clean up of Dialogue code

--- W.I.P/Assets/dialogueScriptTest01.cs
+++ W.I.P/Assets/dialogueScriptTest01.cs
@@ -6,8 +6,11 @@ public class dialogueScriptTest01 : MonoBehaviour
 {
     public bool IsStartingStatement;
     public GameObject dialogueOption1;
+    public string statementOption1;
     public GameObject dialogueOption2;
+    public string statementOption2;
     public GameObject dialogueOption3;
+    public string statementOption3;
     void Start()
     {
         Statement();
@@ -18,22 +21,22 @@ public class dialogueScriptTest01 : MonoBehaviour
     {
         if (IsStartingStatement == false)
         {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
 
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+
+            }
         }
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-
-        }
+        
 
 
 
