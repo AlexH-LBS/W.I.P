9f83f95a2848014270bfd3aabd81dcad727da2bb
AlexH-LBS <alexander.holmstedt@elev.ga.lbs.se>
2024-12-17 20:11:58 +0100

    Start on rotate/made a sacreficial scene

--- W.I.P/Assets/BusMovement.cs
+++ W.I.P/Assets/BusMovement.cs
@@ -16,24 +16,24 @@ public class BusMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //bus.velocity = bus.velocity.normalized;
+        bus.velocity = bus.velocity.normalized;
         
         //temporary movement code
         if (Input.GetKey(KeyCode.W))
         {
-            bus.velocity += new Vector2(0, 0.05f);
+            bus.velocity = new Vector2(0, 1);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            bus.velocity += new Vector2(0, -0.05f);
+            bus.velocity = new Vector2(0, -1);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            bus.velocity += new Vector2(-0.05f,0);
+            transform.Rotate(0, 0, 1);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            bus.velocity += new Vector2(0.05f,0);
+            transform.Rotate(0, 0, -1);
         }
 
         if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.D))
